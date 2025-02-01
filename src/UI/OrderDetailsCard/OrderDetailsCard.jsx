f7a580574e7ae525a9efe24df145a2d057c71310
Gayathriravi31 <gayathriravi3105@gmail.com>
2025-02-01 08:45:33 -0800

    deploy issue fix

--- src/UI/OrderDetailsCard/OrderDetailsCard.jsx
+++ src/UI/OrderDetailsCard/OrderDetailsCard.jsx
@@ -6,7 +6,7 @@ const OrderDetailsCard = () => {
     <div className='order_details_container'>
       <div className='order_details_card_header'>
         <h3 className='common_header_h3'>Order Details</h3>
-        <a className='order_details_card_link'>view All</a>
+        <a className='order_details_card_link' href='/'>view All</a>
       </div>
       <div className='order_details_card_grid'>
         {
@@ -16,7 +16,7 @@ const OrderDetailsCard = () => {
                 <h4 className='order_details_header'>18</h4>
                 <p className='total_text'>Total Orders</p>
                 <div className='icon_flex'>
-                  <span className='trading_icon_box'><img src='' className='trading_icon' /></span>
+                  <span className='trading_icon_box'><img src='' className='trading_icon'alt='trading icon'  /></span>
                   <span>+24%</span>
                 </div>
               </div>
